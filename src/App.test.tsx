76b7c961e1bbc657d59bf9108b15adafc5684815
Peter Goodey <goodey.peter@gmail.com>
2024-07-24 18:07:28 +0100

    Add renovate and use USerEvents in the new way

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,8 +5,9 @@ import { render, screen, userEvent } from './test/custom-renderer'
 describe('Simple working test', () => {
 
   it('should increment count on click', async () => {
+    const user = userEvent.setup();
     render(<App />)
-    userEvent.click(screen.getByRole('button'))
+    await user.click(screen.getByRole('button'))
     expect(await screen.findByText(/count is 1/i)).toBeVisible()
   })
 })
